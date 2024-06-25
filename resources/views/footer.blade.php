f2a835e0495f62a4bcfb309f897a89db73c4e2cd
bebcg111K <ekhramdifa045@gmail.com>
2024-06-25 11:01:18 +0700

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,20 @@
+<div class="w-full bg-develobe-900 h-[320px] text-white flex flex-row pt-12">
+  <div class="w-6/12 pl-28 flex flex-col">
+    <span class="font-quicksand text-4xl font-bold">CinemaE1</span>
+    <span class="font-inter text-lg mt-4">WEB ini hanya untuk project akhir<br>yang bisa insyaallah membuat nilai kami jadi A , hehe<br>#BetterMovieBetterLife</span>
+    <span class="font-inter text-lg mt-4">&copy; 2024 CINEMATION</span>
+  </div>
+
+  <div class="w-3/12 flex flex-col">
+    <span class="font-inter font-bold text-lg">Website</span>
+    <a href="/" class="font-inter text-lg mt-4 hover:text-develobe-500 duration-200">Home</a>
+    <a href="/movies" class="font-inter text-lg mt-4 hover:text-develobe-500 duration-200">Movies</a>
+    <a href="/tv-shows" class="font-inter text-lg mt-4 hover:text-develobe-500 duration-200">TV Shows</a>
+  </div>
+  
+  <div class="w-3/12 flex flex-col">
+    <span class="font-inter font-bold text-lg">Social</span>
+    <a href="https://www.instagram.com/develobe.education/" target="_blank" class="font-inter text-lg mt-4 hover:text-develobe-500 duration-200">Instagram</a>
+    
+  </div>
+</div>
